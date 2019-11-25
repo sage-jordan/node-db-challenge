0a8acdbc1ce35dd4223c3ee3228a36b74e151498
sage-jordan <sagemjrodan@gmail.com>
2019-11-24 22:13:06 -0500

    router and server setup

---
+++ index.js
@@ -0,0 +1,23 @@
+// SERVER SETUP
+const express = require('express');
+const server = express();
+
+// IMPORT ROUTERS
+const PR = require('./data/routers/projectRouter');
+const TR = require('./data/routers/taskRouter');
+const RR = require('./data/routers/resourceRouter');
+
+// USE ROUTERS
+server.use('/project', PR);
+server.use('/tasks', TR);
+server.use('/resource', RR);
+
+// DEFAULT ROUTE
+server.get('/', (req, res) => {
+    res.send(`The server works!`);
+});
+
+// SERVER LISTEN
+server.listen(4000, () => {
+    console.log(`===server listening on port 4000===`);
+});
